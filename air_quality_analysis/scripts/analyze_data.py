9c1bf91198d71e216bba7f95f4a93e734f00d9a7
ttasnim5 <tahsintasnim6@gmail.com>
2025-01-16 17:43:05 -0500

    added more tests and visualizations, primarily categorization

--- air_quality_analysis/scripts/analyze_data.py
+++ air_quality_analysis/scripts/analyze_data.py
@@ -9,6 +9,15 @@ def combine_datasets(output_file='./data/merged_data.csv'):
     # extract year from OpenAQ data using 'period.datetimeFrom.local'
     aq_data['Year'] = pd.to_datetime(aq_data['period.datetimeFrom.local']).dt.year
     aq_data = aq_data.rename(columns={'value': 'PM10'})
+    # drop columns from dataframe that are irrelevant/unhelpful
+    aq_data.drop(['flagInfo.hasFlags', 'parameter.id', 
+                'period.datetimeFrom.utc', 'period.datetimeFrom.local', 'period.datetimeTo.utc',
+                'period.datetimeTo.local', 'summary.min', 'summary.q02', 'summary.q25', 'summary.median',
+                'summary.q75', 'summary.q98', 'summary.max', 'summary.avg', 'summary.sd', 
+                'coverage.expectedCount', 'coverage.expectedInterval', 'coverage.observedCount', 
+                'coverage.observedInterval', 'coverage.percentComplete', 'coverage.percentCoverage', 
+                'coverage.datetimeFrom.utc', 'coverage.datetimeFrom.local', 'coverage.datetimeTo.utc', 
+                'coverage.datetimeTo.local', ], axis=1, inplace=True)
     # merge datasets on Year // inner join to only include years present in both datasets
     merged_data = pd.merge(wb_data, aq_data, on='Year', how='inner')
 
